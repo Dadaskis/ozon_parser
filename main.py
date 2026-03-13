63f4bbeae6d6b724c267fba27bf458009aa8bb6c
Dadaskis <dadaskis@gmail.com>
2026-03-13 10:57:20 +0300

    Screwed up experimenting

---
+++ main.py
@@ -0,0 +1,42 @@
+from playwright.async_api import async_playwright
+from playwright_stealth import Stealth
+import asyncio
+
+async def main():
+    print("Main - Start")
+    
+    stealth = Stealth()
+    #stealth.
+
+    async with stealth.use_async(async_playwright()) as playwright:
+    #async with async_playwright() as playwright:
+        browser = await playwright.chromium.launch(
+            #headless=False,
+            args=[
+                "--disable-blink-features=AutomationControlled",
+                "--disable-features=UserAgentClientHint",
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--disable-gpu",
+                "--disable-extensions",
+                "--disable-setuid-sandbox",
+                "--window-size=1920,1080"
+            ]
+        )
+        page = await browser.new_page()
+        await page.goto("https://ozon.by/")
+        await asyncio.sleep(20.0)
+        #await page.goto("https://www.whatismybrowser.com/")
+        #await page.goto("chrome://version")
+        content = await page.content()
+        with open("output1.html", "w", encoding="utf-8") as file:
+            file.write(content)
+        #print("Press Enter to die :)")
+        #input()
+        await browser.close()
+
+    print("Main - End")
+
+if __name__ == "__main__":
+    asyncio.run(main())
